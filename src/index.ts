3dcbdf670ca0e3f6eec99ea292364045e382bd76
akessner <akessner@gmail.com>
2021-03-20 22:13:26 +0200

    add test program

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Pipeline } from "./pipeline"
+
+const P = Pipeline
+P.stdinSource(
+    P.filter((i)=>{return i>0}, 
+        P.fixedEventWindow(2, 
+            P.foldSum([],
+                P.fixedEventWindow(3, 
+                    P.foldMedian([],
+                        P.stdoutSink()
+                    )
+                )
+            )
+        )
+    )
+)
