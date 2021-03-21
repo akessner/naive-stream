aa2df32fd448c66840a9e455c148c3371c3ffb77
akessner <akessner@gmail.com>
2021-03-21 19:27:43 +0200

    remove unecessary tersness

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,67 +1,73 @@
 export class Pipeline {
-  static filter(predicate: (i: any) => boolean, fn: (i: any) => void): (i: any) => void {
+  static stdinSource(nextTransformation: (input: number) => void) {
+    if (process.env.NODE_ENV !== "test") {
+      process.stdin.on("readable", () => printInputToStdOut(nextTransformation));
+    }
     return (i) => {
-      if (predicate(i)) {
-        fn(i);
+      console.log(`> ${i}`);
+      nextTransformation(i);
+    };
+    function printInputToStdOut(nextTransformation: (input: number) => void) {
+      let chunk;
+      while ((chunk = process.stdin.read()) !== null) {
+        process.stdout.write(`> ${chunk}\n`);
+        nextTransformation(parseInt(chunk));
       }
+    }
+  }
+
+  static stdoutSink(nextTransformation: (input: number) => void = () => {}) {
+    return (input) => {
+      if (process.env.NODE_ENV === "test") {
+        console.log(input);
+      } else {
+        process.stdout.write(`${input}\n`);
+      }
+      nextTransformation(input);
     };
   }
 
-  static foldMedian(arr: number[]): number {
-    const len = arr.length;
-    if (len % 2 === 0) {
-      return sumTwoMiddleValues(arr, len) / 2;
-    } else {
-      const middleIndex = (len - 1) / 2;
-      return arr[middleIndex];
-    }
-    function sumTwoMiddleValues(arr: number[], len: number) {
-      return arr[len / 2] + arr[len / 2 - 1] || 0;
-    }
-  }
-  static foldSum(arr: number[]): number {
-    return arr.reduce((prev, i) => prev + i, 0);
+  static filter(
+    predicate: (input: number) => boolean,
+    nextTransformation: (input: number) => void
+  ): (input: number) => void {
+    return (input) => {
+      if (predicate(input)) {
+        nextTransformation(input);
+      }
+    };
   }
 
   static fixedEventWindow(
     windowLength: number,
     foldStrategy: (arr: number[]) => number | number[],
-    fn: (i) => void,
+    nextTransformation: (input) => void,
     recursiveArray: number[] = []
-  ): any {
-    return (i) => {
-      recursiveArray.push(parseInt(i));
+  ): (input: any) => void {
+    return (input) => {
+      recursiveArray.push(parseInt(input));
       if (recursiveArray.length < windowLength) {
-        return this.fixedEventWindow(windowLength, foldStrategy, fn, recursiveArray);
+        return this.fixedEventWindow(windowLength, foldStrategy, nextTransformation, recursiveArray);
       }
-      fn(foldStrategy(recursiveArray));
+      nextTransformation(foldStrategy(recursiveArray));
       recursiveArray = [];
     };
   }
 
-  static stdoutSink(fn: (i) => void = (i) => {}) {
-    return (i) => {
-      if (process.env.NODE_ENV === "test") {
-        console.log(i);
-      } else {
-        process.stdout.write(`${i}\n`);
-      }
-      fn(i);
-    };
+  static foldSum(arr: number[]): number {
+    return arr.reduce((prev, i) => prev + i, 0);
   }
-  static stdinSource(fn: (i) => void) {
-    if (process.env.NODE_ENV !== "test") {
-      process.stdin.on("readable", () => {
-        let chunk;
-        while ((chunk = process.stdin.read()) !== null) {
-          process.stdout.write(`> ${chunk}\n`);
-          fn(parseInt(chunk));
-        }
-      });
+
+  static foldMedian(arr: number[]): number {
+    const length = arr.length;
+    if (length % 2 === 0) {
+      return sumTwoMiddleValues(arr, length) / 2;
+    } else {
+      const middleIndex = (length - 1) / 2;
+      return arr[middleIndex];
     }
-    return (i) => {
-      console.log(`> ${i}`);
-      fn(i);
-    };
+    function sumTwoMiddleValues(arr: number[], length: number) {
+      return arr[length / 2] + arr[length / 2 - 1] || 0;
+    }
   }
 }
